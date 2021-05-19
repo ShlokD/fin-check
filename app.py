34e57ebc046f94872be260bfb2f5db4c9e147d32
Shlok Desai <shlokpdesai@gmail.com>
2021-05-19 19:00:17 +0530

    add db connection

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from datetime import datetime
 from flask import Flask, jsonify
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
+
+def connect_to_db():
+    db_conn = os.environ["ATLAS_URI"]
+    client = MongoClient(db_conn)
+    db = client["fin-check"]
+    return db
+
 @app.route("/")
 def index():
-    return jsonify({"msg": "Namaste Samsara", "time": datetime.utcnow().isoformat(sep='-')})
+    db = connect_to_db()
+    users_from_db = db.users.find()
+    users = []
+    for user in users_from_db:
+        users.append({"name": user["name"]})
+
+    return jsonify({"users": users, "time": datetime.utcnow().isoformat(sep='-')})
+
 
 if __name__ == '__main__':
    app.run(threaded=True, port=5000)
