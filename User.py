2769e2a088cc6c9bdec41063d69e1de479093686
Prarthana <prarthanashah791@gmail.com>
2021-05-23 17:36:08 +0530

    Changes for user entry in db

--- User.py
+++ User.py
@@ -1,8 +1,28 @@
-def make_user(inpur):
+import random
+import string
+
+def make_user(entry):
     return {
-        "name": input["name"]
+        "name": entry["name"]
     }
 
 
-def make_user_entry():
-    pass
+def make_user_entry(entry):
+    id = make_autogenerate_id(6)
+    db_data = {
+    "id": id,
+    "name": entry["name"].title(),
+    "department":entry["department"].title(),
+    "location": entry["location"].title()
+    }
+    return db_data
+
+def make_autogenerate_id(length):
+    id =''
+    for value in range(length):
+        if value == 0:
+            id += random.choice(string.ascii_uppercase)
+        else:
+            id += random.choice(string.digits)
+    print(id)
+    return id
--- UserTest.py
+++ UserTest.py
@@ -1,12 +1,34 @@
 import unittest
-from User import make_user
+from User import *
 
 class UserTests(unittest.TestCase):
     def test_make_user(self):
-        expected = { "name": "Jon Snow"}
+        expected = {"name": "Jon Snow"}
         actual = make_user({"name": "Jon Snow"})
         self.assertEqual(expected, actual)
 
+    def test_user_entry(self):
+        expected_name = "Ganguram Sai"
+        expected_department = "Administration"
+        expected_location = "Kashi"
+
+        actual = make_user_entry({
+        "name": "Ganguram Sai",
+        "department": "Administration",
+        "location": "Kashi"
+        })
+        actual_name = actual["name"]
+        actual_department = actual["department"]
+        actual_location = actual["location"]
+        self.assertEqual(expected_name,actual_name)
+        self.assertEqual(expected_department, actual_department)
+        self.assertEqual(expected_location, actual_location)
+
+    def test_user_id(self):
+        actual_id = make_autogenerate_id(6)
+        self.assertEqual(len(actual_id),6)
+        self.assertEqual(actual_id[0].isupper(),True)
+        self.assertEqual(actual_id[1:].isdecimal(),True)
 
 if __name__ == '__main__':
     unittest.main()
