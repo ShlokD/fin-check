d3c5b322fb8f12ee6137b208b50addcb82e06bc5
Shlok Desai <shlokpdesai@gmail.com>
2021-06-12 16:57:52 +0530

    Create navigation

--- ui/src/App.js
+++ ui/src/App.js
@@ -11,13 +11,19 @@ const Transactions = lazy(() => import("./Transactions"));
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="h-full">
       <Suspense fallback={<div>Loading...</div>}>
         <BrowserRouter history={createBrowserHistory()}>
-          <Navigation />
-          <Route path="/" exact component={Cards}></Route>
-          <Route path="/people" component={People}></Route>
-          <Route path="/transactions" component={Transactions}></Route>
+          <div className="flex lg:flex-row flex-col h-full">
+            <div className="w-full lg:w-1/4">
+              <Navigation />
+            </div>
+            <div className="w-full lg:w-3/4">
+              <Route path="/" exact component={Cards}></Route>
+              <Route path="/people" component={People}></Route>
+              <Route path="/transactions" component={Transactions}></Route>
+            </div>
+          </div>
         </BrowserRouter>
       </Suspense>
     </div>
--- ui/src/Navigation/index.js
+++ ui/src/Navigation/index.js
@@ -1,21 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import creditcard from "../images/credit-card.png";
+import group from "../images/group.png";
+import transaction from "../images/transaction.png";
+import money from "../images/money.png";
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Cards</Link>
-        </li>
-        <li>
-          <Link to="/people">People</Link>
-        </li>
-        <li>
-          <Link to="/transactions">Transactions</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="border-r-2 border-gray-200 w-full h-full p-6">
+      <div className="flex items-center lg:justify-start justify-center border-b-2 border-gray-200 p-6">
+        <img alt="Fin Check Logo" height={100} width={100} src={money}></img>
+        <h1 className="text-3xl font-bold p-4">Fin Check</h1>
+      </div>
+
+      <nav className="text-2xl font-bold w-full h-full my-6">
+        <ul className="w-full h-full flex flex-col md:flex-row lg:flex-col items-center md:items-start md:justify-center lg:justify-start">
+          <li className="p-4">
+            <Link className="flex items-center w-full" to="/">
+              <img
+                alt="Credit Cards"
+                src={creditcard}
+                height={50}
+                width={50}
+              ></img>
+              <p className="p-4">Cards</p>
+            </Link>
+          </li>
+          <li className="p-4">
+            <Link className="flex items-center w-full" to="/people">
+              <img alt="People" src={group} height={50} width={50}></img>
+              <p className="p-4">People</p>
+            </Link>
+          </li>
+          <li className="p-4">
+            <Link className="flex items-center w-full" to="/transactions">
+              <img
+                alt="Transactions"
+                src={transaction}
+                height={50}
+                width={50}
+              ></img>
+              <p className="p-4">Transactions</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
